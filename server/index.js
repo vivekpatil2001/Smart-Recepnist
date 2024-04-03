51b8dc12a90c3c55fbc1b44f84f501ff9a359fde
vivekpatil2001 <vivekshejole143@gmail.com>
2024-04-03 13:09:23 +0530

    added face detection

--- server/index.js
+++ server/index.js
@@ -491,7 +491,7 @@ const transporter = nodemailer.createTransport({
 
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`The server is Running on Port ${PORT} 🚀`);
